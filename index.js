3038a6f9ac436e739136f42fdcbbe6fc211811e3
lyndsclev <lyndsclev@gmail.com>
2022-03-23 15:56:05 -0400

    added pseudocode to index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+// required
+
+const fs = require('fs'); 
+const inquirer = require('inquirer');
+
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer'); 
+const Intern = require('./lib/Intern'); 
+
+const teamArr = []; 
+
+// manager prompts 
+
+// push manager to teamArr 
+
+// add employee or finish team option 
+
+// engineer prompts
+
+// push manager to teamArr 
+
+// intern prompts 
+
+// push manager to teamArr 
+
+// write HTML file using template 
+
+
