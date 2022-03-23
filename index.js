20525c621d50191eab44cd7ec4f297ba010201d5
lyndsclev <lyndsclev@gmail.com>
2022-03-23 17:30:59 -0400

    questions built for all employee types

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 // required
-
 const fs = require('fs'); 
 const inquirer = require('inquirer');
 
@@ -7,6 +6,7 @@ const Manager = require('./lib/Manager');
 const Engineer = require('./lib/Engineer'); 
 const Intern = require('./lib/Intern'); 
 
+// team array 
 const teamArr = []; 
 
 // manager prompts 
@@ -71,28 +71,172 @@ const addManager = () => {
     .then(data => {
         const { name, id, email, officeNumber } = data; 
         teamArr.push(new Manager(name, id, email, officeNumber));
+        
+        // confirm manager is added to teamArr 
+        console.log(teamArr); 
+    }); 
+};
+
+// add employee or finish team option 
+const buildTeam = () => {
+    return inquirer.prompt([
+        {
+            type: 'list', 
+            name: 'newTeamMember', 
+            message: 'would you like to add another team member?',
+            choices: ['Yes, add an Engineer', 'Yes, add an Intern', 'No, the team is complete!']
+        }
+    ]) .then(answer => {
+        if(answer.newTeamMember === 'Yes, add an Engineer') {
+            addEngineer(); 
+        } else if (answer.newTeamMember === 'Yes, add an Intern') {
+            addIntern(); 
+        } else {
+
+            // write HTML file using template 
+        }
     });
 };
 
-
-
-// add employee or finish team option 
-
-const buildTeam = () => {}
-
 // engineer prompts
+const addEngineer = () => {
+    return inquirer.prompt([
+        {
+            type: 'text', 
+            name: 'name', 
+            message: "Enter the engineer's name (required).", 
+            validate: engineerName => {
+                if(engineerName) {
+                    return true; 
+                } else {
+                    console.log("Please enter the engineer's name."); 
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'text', 
+            name: 'id', 
+            message: "Enter the engineer's ID number (required).",
+            validate: engineerId => {
+                if(engineerId) {
+                    return true; 
+                } else {
+                    console.log("Please enter the engineer's ID number."); 
+                    return false; 
+                }
+            }
 
-const addEngineer = () => {}
+        }, 
+        {
+            type: 'text', 
+            name: 'email', 
+            message: "Enter the engineer's email address (required).", 
+            validate: engineerEmail => {
+                if(engineerEmail) {
+                    return true; 
+                } else {
+                    console.log("Please enter the engineer's email address."); 
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'text', 
+            name: 'github', 
+            message: "Enter the engineer's GitHub username (required).", 
+            validate: engineerGithub => {
+                if(engineerGithub) {
+                    return true; 
+                } else {
+                    console.log("Please enter the engineer's GitHub username."); 
+                    return false; 
+                }
+            }
+        },
+    ]) 
 
-// push engineer to teamArr 
+    // push engineer to teamArr
+    .then(data => {
+        const { name, id, email, github } = data; 
+        teamArr.push(new Engineer(name, id, email, github));
+        
+        // confirm engineer is added to teamArr 
+        console.log(teamArr); 
+    }); 
+};
+
 
 // intern prompts 
+const addIntern = () => {
+    return inquirer.prompt([
+        {
+            type: 'text', 
+            name: 'name', 
+            message: "Enter the intern's name (required).", 
+            validate: internName => {
+                if(internName) {
+                    return true; 
+                } else {
+                    console.log("Please enter the intern's name."); 
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'text', 
+            name: 'id', 
+            message: "Enter the intern's ID number (required).",
+            validate: internId => {
+                if(internId) {
+                    return true; 
+                } else {
+                    console.log("Please enter the intern's ID number."); 
+                    return false; 
+                }
+            }
 
-const addIntern = () => {}
+        }, 
+        {
+            type: 'text', 
+            name: 'email', 
+            message: "Enter the intern's email address (required).", 
+            validate: internEmail => {
+                if(internEmail) {
+                    return true; 
+                } else {
+                    console.log("Please enter the intern's email address."); 
+                    return false; 
+                }
+            }
+        },
+        {
+            type: 'text', 
+            name: 'school', 
+            message: "Enter the intern's school (required).", 
+            validate: internSchool => {
+                if(internSchool) {
+                    return true; 
+                } else {
+                    console.log("Please enter the intern's school."); 
+                    return false; 
+                }
+            }
+        },
+    ]) 
 
-// push intern to teamArr 
+    // push intern to teamArr
+    .then(data => {
+        const { name, id, email, school } = data; 
+        teamArr.push(new Intern(name, id, email, school));
+        
+        // confirm intern is added to teamArr 
+        console.log(teamArr); 
+    }); 
+}
 
-// write HTML file using template 
+
+
 
 // call function to start application 
 addManager()
